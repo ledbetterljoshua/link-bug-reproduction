d541b67a173e94dda319322941a1cc4d6bfd17c5
joshua ledbetter <ledbetterljoshua@gmail.com>
2024-11-16 15:22:46 +0300

    initial commit: setup next.js project

--- app/another/page.tsx
+++ app/another/page.tsx
@@ -5,8 +5,7 @@ export default function Page() {
     <div className="flex-center flex-col p-6">
       <h1 className="text-2xl mb-10">Link Bug</h1>
       <div className="flex flex-col item-center justify-center gap-10">
-        <Link href="/another?link=1">link 1</Link>
-        <Link href="/another?link=2">link 2</Link>
+        <Link href="/">back home</Link>
       </div>
     </div>
   );
