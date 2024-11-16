731716ef454055a6ed4e2c227c9485d252526762
joshua ledbetter <ledbetterljoshua@gmail.com>
2024-11-16 15:37:34 +0300

    adds suspense

--- app/app-route/page.tsx
+++ app/app-route/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Content from "./content";
 
 export default function Page() {
-  return <Content />;
+  return (
+    <Suspense fallback={<div />}>
+      <Content />
+    </Suspense>
+  );
 }
