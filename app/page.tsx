04dc0e272e54339f49f0466d942d3fe0c7d451a2
joshua ledbetter <ledbetterljoshua@gmail.com>
2024-11-18 19:21:31 +0200

    prefetch false

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ export default function Page() {
     <div className="flex-center flex-col p-6">
       <h1 className="text-2xl mb-10">Link Bug</h1>
       <div className="flex flex-col item-center justify-center gap-4">
-        <Link href="/pages-route?link=1">pages route link 1 - works</Link>
-        <Link href="/pages-route?link=2">
+        <Link prefetch={false} href="/pages-route?link=1">
+          pages route link 1 - works
+        </Link>
+        <Link prefetch={false} href="/pages-route?link=2">
           pages route link 2 - broken: query param defaults to link 1
         </Link>
-        <Link className="mb-4" href="/pages-route?link=3">
+        <Link prefetch={false} className="mb-4" href="/pages-route?link=3">
           pages route link 3 - broken: query param defaults to link 1
         </Link>
         <Link href="/app-route?link=2">app-route link 1 - works</Link>
